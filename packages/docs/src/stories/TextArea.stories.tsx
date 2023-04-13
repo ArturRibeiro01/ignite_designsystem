ffd31954a8380ea28c52c1c56abb7d74f30087ac
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-04-12 23:21:59 -0300

    :construction:TextArea

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,36 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm" css={{ color: '$white' }}>
+            Observations
+          </Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Add Any Observations',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
