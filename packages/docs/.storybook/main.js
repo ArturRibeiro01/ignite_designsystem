a8ea923e948f9d75ec362c449a540d1195988ac3
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-04-13 22:57:55 -0300

    :fix: correção para deploy

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,5 +17,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite_designsystem/'
+    }
+    return config
+  }
 };
 export default config;
