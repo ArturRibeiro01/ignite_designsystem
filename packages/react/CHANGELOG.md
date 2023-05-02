ba917fd0b98aefae131402e48a63b0c441008646
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-05-02 01:15:55 -0300

    Ajustes e correçoes no reactjs

---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @rocketseat-aulas-ignite-ui/react
+
+## 1.0.1
+
+### Patch Changes
+
+- correçoes de react para futuros projetos
--- packages/react/src/components/TextInput/styles.ts
+++ packages/react/src/components/TextInput/styles.ts
@@ -7,7 +7,22 @@ export const TextInputContainer = styled('div', {
   boxSizing: 'border-box',
   border: '2px solid $gray900',
   display: 'flex',
-  alignItems: 'baseline',
+  alignItems: 'center',
+
+  variants: {
+    size: {
+      sm: {
+        padding: '$2 $3',
+      },
+      md: {
+        padding: '$3 $4',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
 
   '&:has(input:focus)': {
     borderColor: '$ignite300',
@@ -43,7 +58,7 @@ export const Input = styled('input', {
     cursor: 'not-allowed',
   },
 
-  '&:placeholder': {
+  '&::placeholder': {
     color: '$gray400',
   },
 })
